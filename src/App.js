b25a9c5d516eec46417c7a664fb6da40e5ae6f1d
Fiona Attah <princessfiona111@gmail.com>
2023-02-11 17:47:05 -0500

    movie change

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import SearchIcon from './search.svg';
  
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c4df8ac0";
 
-const movie1 = {
+/*const movie1 = {
     "Title": "The Amazing Spiderman 2 Webb Cut",
     "Year": "2021",
     "imdbID": "tt18351128",
     "Type": "movie",
     "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
-}
+} */
 
 const App = () => {
   const [movies, setMovies ] = useState([]);
